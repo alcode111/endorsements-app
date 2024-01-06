9cc62b018d39e546b20b6ffeefdef4033d58fd6d
Ismail <141173099+alcode111@users.noreply.github.com>
2024-01-06 14:42:29 +0100

    Initialize variables for future DOM manipulation

--- index.js
+++ index.js
@@ -9,3 +9,9 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const endorsementsInDB = ref(database, "endorsements")
+
+const textAreaEl = document.getElementById("endorsement-textarea")
+const fromInput = document.getElementById("from-input")
+const toInput = document.getElementById("to-input")
+const publishButton = document.getElementById("publish-btn")
+const endorsementsUL = document.getElementById("endorsements-ul")
